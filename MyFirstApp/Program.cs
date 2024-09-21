21a1f1c3b8fca637da213f295868d34aa81b170a
MingiLamp. <olari.reitel@tktk.ee>
2024-09-21 19:48:17 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment for my code and I can write what I like here
+//Rakendus kysib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat
+
+
+
+Console.WriteLine("Enter your name");
+//sting - sõne
+string userName =Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
